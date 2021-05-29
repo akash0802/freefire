96c097e3a34eaa556c3da0917e0328ce99db5bc0
akash0802 <gakash781@gmail.com>
2021-05-29 15:20:07 +0530

    bootstrap

--- index.php
+++ index.php
@@ -7,23 +7,59 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reward</title>
     <link type="text/css" href="free.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
 
 </head>
 
 <body>
-    <div align="right">
-        <h1>Reward Redemption Site</h1>
+<div class="container-fluid text-white opacity-2">
+    <br><br>
+    <div class="row justify-content-end">
+        <div class="col-5">
+            <h3>Reward Redemption Site</h3>
+        </div>
     </div>
-    <div>
-        <form action="login.php" type="POST">
-            <b>email id</b> &nbsp; <input name="email" type="text">
-            <br>
-            <br>
-            <b>password</b> <input name="pass" type="password">
-            <br>
-            <button type="submit">Login</button>
-        </form>
+    
+    <br>
+    <div class="row justify-content-end">
+        <div class="col-6">
+        </div>
+        <div class="col-4">
+            <div>
+            <form action="login.php" type="POST">
+                <b>email id</b> &nbsp; <input name="email" type="text">
+                <br>
+                <br>
+                <b>password</b> <input name="pass" type="password">
+                <br>
+                <br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                <button type="submit">Login</button>
+            </form>
+            </div>
+        </div>
+        <div class="col-2">
+
+        </div>
     </div>
+    <br><br>
+    <div class="row justify-content-end">
+        <div class="col-5 small" style="background-color:rgba(0,0,0,0.4);">
+        Important Notice:<br>
+1. Redemption code has 12 characters, consisting of capital letters and numbers.<br>
+2. Item rewards are shown in [vault] tab in game lobby; Golds or diamonds will add in account wallet automatically.<br>
+3. Please note redemption expiration date. Any expired codes cannot be redeemed.<br>
+4. Please contact customer service if you encountered any issue.<br>
+5. Reminder: you will not be able to redeem your rewards with guest accounts. You may bind your account to Facebook or VK in order to receive the rewards.<br>
+        </div>
+        <div class="col-1">
+
+        </div>
+    </div>
+
+
+</div>
+    
 
 </body>
 
---
+++ index3.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reward</title>
+    <link type="text/css" href="free.css" rel="stylesheet">
+    
+
+</head>
+
+<body>
+    <div align="right">
+        <h1>Reward Redemption Site</h1>
+    </div>
+    <div>
+        <form action="login.php" type="POST">
+            <b>email id</b> &nbsp; <input name="email" type="text">
+            <br>
+            <br>
+            <b>password</b> <input name="pass" type="password">
+            <br>
+            <button type="submit">Login</button>
+        </form>
+    </div>
+
+</body>
+
+</html>
